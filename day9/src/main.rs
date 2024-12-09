515baab231dff3d223c1ca4d0f8ce83b5cdb4fce
David Lang <davidlang42@gmail.com>
2024-12-09 17:19:46 +1100

    Revert "so far"
    This reverts commit e8ded9d4a882dc098e9328bb533da8af6a01e7a7.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -5,28 +5,12 @@ use std::fmt::Display;
 use std::fmt::Formatter;
 
 struct DiskMap {
-    used: Vec<File>,
+    used: Vec<usize>,
     free: Vec<usize> // free.len==used.len()-1
 }
 
 struct Disk {
     blocks: Vec<Option<Block>>
-}
-
-struct DiskByFile {
-    files: Vec<FileOrSpace>,
-    max_id: usize
-}
-
-enum FileOrSpace {
-    File(File),
-    Space(usize)
-}
-
-#[derive(Copy, Clone)]
-struct File {
-    id: usize,
-    size: usize
 }
 
 struct Block {
@@ -41,13 +25,8 @@ impl FromStr for DiskMap {
         let mut free = Vec::new();
         let digits: Vec<usize> = line.chars().map(|c| c.to_string().parse().unwrap()).collect();
         let mut digits_iter = digits.iter();
-        let mut id = 0;
         loop {
-            used.push(File {
-                id,
-                size: *digits_iter.next().unwrap()
-            });
-            id += 1;
+            used.push(*digits_iter.next().unwrap());
             if let Some(next) = digits_iter.next() {
                 free.push(*next);
             } else {
@@ -83,31 +62,18 @@ impl DiskMap {
             blocks: Vec::new()
         };
         for i in 0..self.used.len() - 1 {
-            disk.add_file(self.used[i]);
+            disk.add_file(self.used[i], i);
             disk.add_space(self.free[i]);
         }
-        disk.add_file(self.used[self.used.len() - 1]);
-        disk
-    }
-
-    fn to_disk_by_file(&self) -> DiskByFile {
-        let mut disk = DiskByFile {
-            files: Vec::new(),
-            max_id: 0
-        };
-        for i in 0..self.used.len() - 1 {
-            disk.add_file(self.used[i]);
-            disk.add_space(self.free[i]);
-        }
-        disk.add_file(self.used[self.used.len() - 1]);
+        disk.add_file(self.used[self.used.len() - 1], self.used.len() - 1);
         disk
     }
 }
 
 impl Disk {
-    fn add_file(&mut self, file: File) {
-        for _i in 0..file.size {
-            self.blocks.push(Some(Block { id: file.id }));
+    fn add_file(&mut self, size: usize, id: usize) {
+        for _i in 0..size {
+            self.blocks.push(Some(Block { id }));
         }
     }
 
@@ -146,67 +112,6 @@ impl Disk {
     }
 }
 
-impl DiskByFile {
-    fn add_file(&mut self, file: File) {
-        self.files.push(FileOrSpace::File(file));
-        if file.id > self.max_id {
-            self.max_id = file.id;
-        }
-    }
-
-    fn add_space(&mut self, size: usize) {
-        self.files.push(FileOrSpace::Space(size));
-    }
-
-    fn compact(&mut self) {
-        let mut move_from = self.files.len() - 1;
-        let mut move_to = 0;
-        for id in (0..(self.max_id + 1)).rev() {
-            let mut required_size = 0;
-            let move_from = self.files.iter().position(|f| {
-                if let FileOrSpace::File(file) = f {
-                    required_size = file.size;
-                    file.id == id
-                } else {
-                    false
-                }
-            }).unwrap();
-            for move_to in (0..move_from).rev() {
-                if let FileOrSpace::Space(size) = self.files[move_to] && size >= required_size {
-                    
-                }
-            }
-        }
-
-        while move_from > move_to {
-            if self.files[move_from].is_none() {
-                move_from -= 1;
-                continue;
-            }
-            if self.blocks[move_to].is_some() {
-                move_to += 1;
-                continue;
-            }
-            self.blocks.swap(move_from, move_to);
-            move_to += 1;
-            move_from -= 1;
-        }
-    }
-
-    fn to_disk(&self) -> Disk {
-        let mut disk = Disk {
-            blocks: Vec::new()
-        };
-        for fs in &self.files {
-            match fs {
-                FileOrSpace::File(f) => disk.add_file(*f),
-                FileOrSpace::Space(s) => disk.add_space(*s)
-            }
-        }
-        disk
-    }
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
@@ -219,10 +124,6 @@ fn main() {
         disk.compact();
         println!("Compact: {}", disk);
         println!("Checksum: {}", disk.checksum());
-        let mut disk_by_file = map.to_disk_by_file();
-        disk_by_file.compact();
-        println!("Compact by file: {}", disk_by_file.to_disk());
-        println!("Checksum by file: {}", disk_by_file.to_disk().checksum());
     } else {
         println!("Please provide 1 argument: Filename");
     }
