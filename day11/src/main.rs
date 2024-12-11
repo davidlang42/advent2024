1cfe39335ac2d52300f6072295e0c6848b9d7081
David Lang <davidlang42@gmail.com>
2024-12-11 17:47:02 +1100

    add caching

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::env;
 use std::str::FromStr;
@@ -19,33 +20,40 @@ impl FromStr for Stones {
 }
 
 impl Stones {
-    fn blink(&self, blinks: usize) -> usize {
+    fn blink(&mut self, blinks: usize) -> usize {
         let mut count = 0;
+        let mut cache = HashMap::new();
         for stone in &self.stones {
-            count += Self::make_stones(*stone, blinks).len();
+            count += Self::count_stones(&mut cache, *stone, blinks);
         }
         count
     }
 
-    fn make_stones(stone: usize, blinks: usize) -> Vec<usize> {
+    fn count_stones(cache: &mut HashMap<(usize, usize), usize>, stone: usize, blinks: usize) -> usize {
         if blinks == 0 {
             // finished
-            vec![stone]
+            1
+        } else if let Some(cached_result) = cache.get(&(stone, blinks)) {
+            // cached
+            *cached_result
         } else {
-            // do a blink
-            if stone == 0 {
-                return Self::make_stones(1, blinks - 1);
-            }
-            let stone_str = stone.to_string();
-            if stone_str.len() % 2 == 0 {
-                let middle = stone_str.len() / 2;
-                let left_stone = stone_str[0..middle].parse().unwrap();
-                let right_stone = stone_str[middle..stone_str.len()].parse().unwrap();
-                let mut result = Self::make_stones(left_stone, blinks - 1);
-                result.append(&mut Self::make_stones(right_stone, blinks - 1));
-                return result;
-            }
-            Self::make_stones(stone * 2024, blinks - 1)
+            // calculate the result of blinks
+            let result = if stone == 0 {
+                Self::count_stones(cache, 1, blinks - 1)
+            } else {
+                let stone_str = stone.to_string();
+                if stone_str.len() % 2 == 0 {
+                    let middle = stone_str.len() / 2;
+                    let left_stone = stone_str[0..middle].parse().unwrap();
+                    let right_stone = stone_str[middle..stone_str.len()].parse().unwrap();
+                    Self::count_stones(cache, left_stone, blinks - 1) + Self::count_stones(cache, right_stone, blinks - 1)
+                } else {
+                    Self::count_stones(cache, stone * 2024, blinks - 1)
+                }
+            };
+            // cache this result
+            cache.insert((stone, blinks), result);
+            result
         }
     }
 }
@@ -57,8 +65,8 @@ fn main() {
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
         let blinks = args[2].parse().unwrap();
-        let stones: Stones = text.parse().unwrap();
-        println!("Count after {} blinks: {}", blinks, stones.blink(blinks));
+        let mut stones: Stones = text.parse().unwrap();
+        println!("Count after {} blinks: {}", blinks, &mut stones.blink(blinks));
     } else {
         println!("Please provide 2 arguments: Filename, Blinks");
     }
