756637245e0386413cf38b04fccc415514295bf1
David Lang <davidlang42@gmail.com>
2024-12-12 20:52:34 +1100

    refactor

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -13,12 +13,6 @@ struct Map {
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
 struct Pos {
-    row: usize,
-    col: usize
-}
-
-#[derive(Copy, Clone, Hash, Eq, PartialEq)]
-struct OutOfMapPos {
     row: isize,
     col: isize
 }
@@ -33,21 +27,24 @@ impl FromStr for Map {
 
     fn from_str(text: &str) -> Result<Self, Self::Err> {
         let plants: Vec<Vec<Plant>> = text.lines().map(|l| l.chars().map(|ch| Plant(ch)).collect()).collect();
-        let row = plants.len() - 1;
-        let col = plants[0].len() - 1;
+        let max = Pos {
+            row:  plants.len() as isize - 1,
+            col: plants[0].len() as isize - 1
+        };
         Ok(Self {
             plants,
-            max: Pos {
-                row,
-                col
-            }
+            max
         })
     }
 }
 
 impl Map {
-    fn at(&self, location: &Pos) -> Plant {
-        self.plants[location.row][location.col]
+    fn at(&self, location: &Pos) -> Option<Plant> {
+        if location.row >= 0 && location.col >= 0 && location.row <= self.max.row && location.col <= self.max.col {
+            Some(self.plants[location.row as usize][location.col as usize])
+        } else {
+            None
+        }
     }
 
     fn regions(&self) -> Vec<Region> {
@@ -73,10 +70,11 @@ impl Map {
 
     fn locate(&self, plant: Plant) -> HashSet<Pos> {
         let mut set = HashSet::new();
-        for row in 0..self.plants.len() {
-            for col in 0..self.plants[row].len() {
-                if self.plants[row][col] == plant {
-                    set.insert(Pos { row, col });
+        for row in 0..(self.max.row + 1) {
+            for col in 0..(self.max.col + 1) {
+                let p = Pos { row, col };
+                if self.at(&p) == Some(plant) {
+                    set.insert(p);
                 }
             }
         }
@@ -87,8 +85,8 @@ impl Map {
 impl Region {
     fn expand(&mut self, start: &Pos, map: &Map) {
         if self.locations.insert(*start) {
-            for adj in start.adjacent(&map.max) {
-                if map.at(&adj) == self.plant {
+            for adj in start.adjacent() {
+                if map.at(&adj) == Some(self.plant) {
                     self.expand(&adj, map);
                 }
             }
@@ -99,16 +97,11 @@ impl Region {
         self.locations.len()
     }
 
-    fn perimeter(&self, map: &Map) -> usize {
+    fn perimeter(&self) -> usize {
         let mut perimeter = 0;
         for l in &self.locations {
-            let l_out_of_map = l.out_of_map();
-            for a in l_out_of_map.adjacent_including_out_of_map() {
-                if let Some(a_in_map) = a.in_map(map) {
-                    if !self.locations.contains(&a_in_map) {
-                        perimeter += 1;
-                    }
-                } else {
+            for a in l.adjacent() {
+                if !self.locations.contains(&a) {
                     perimeter += 1;
                 }
             }
@@ -129,74 +122,25 @@ impl Plant {
 }
 
 impl Pos {
-    fn out_of_map(&self) -> OutOfMapPos {
-        OutOfMapPos {
-            row: self.row as isize,
-            col: self.col as isize
-        }
-    }
-
-    fn adjacent(&self, max: &Pos) -> Vec<Pos> {
-        let mut v = Vec::new();
-        if self.row > 0 {
-            v.push(Self {
+    fn adjacent(&self) -> [Pos; 4] {
+        [
+            Self {
                 row: self.row - 1,
                 col: self.col
-            });
-        }
-        if self.col > 0 {
-            v.push(Self {
+            },
+            Self {
                 row: self.row,
                 col: self.col - 1
-            });
-        }
-        if self.row < max.row {
-            v.push(Self {
+            },
+            Self {
                 row: self.row + 1,
                 col: self.col
-            });
-        }
-        if self.col < max.col {
-            v.push(Self {
+            },
+            Self {
                 row: self.row,
                 col: self.col + 1
-            });
-        }
-        v
-    }
-}
-
-impl OutOfMapPos {
-    fn in_map(&self, map: &Map) -> Option<Pos> {
-        if self.row >= 0 && self.col >= 0 && self.row <= map.max.row as isize && self.col <= map.max.col as isize {
-            Some(Pos {
-                row: self.row as usize,
-                col: self.col as usize
-            })
-        } else {
-            None
-        }
-    }
-
-    fn adjacent_including_out_of_map(&self) -> Vec<Self> {
-        let mut v = Vec::new();
-        v.push(Self {
-            row: self.row - 1,
-            col: self.col
-        });
-        v.push(Self {
-            row: self.row,
-            col: self.col - 1
-        });
-        v.push(Self {
-            row: self.row + 1,
-            col: self.col
-        });
-        v.push(Self {
-            row: self.row,
-            col: self.col + 1
-        });
-        v
+            }
+        ]
     }
 }
 
@@ -211,7 +155,7 @@ fn main() {
         let mut sum = 0;
         for r in regions {
             let area = r.area();
-            let perimeter = r.perimeter(&map);
+            let perimeter = r.perimeter();
             println!("Region '{}' with area {}, perimeter {}", r.plant.0, area, perimeter);
             sum += area * perimeter;
         }
