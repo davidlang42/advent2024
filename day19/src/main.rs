24824ae07e6e528f724f692f0e22ef0bdec2b3ad
David Lang <david.lang@displayr.com>
2024-12-19 16:11:19 +1100

    as str

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -2,47 +2,47 @@ use std::fs;
 use std::env;
 use std::str::FromStr;
 
-enum Color {
-    White,
-    Blue,
-    Black,
-    Red,
-    Green
-}
+// enum Color {
+//     White,
+//     Blue,
+//     Black,
+//     Red,
+//     Green
+// }
 
-impl Color {
-    fn from_char(ch: char) -> Self {
-        match ch {
-            'w' => Self::White,
-            'u' => Self::Blue,
-            'b' => Self::Black,
-            'r' => Self::Red,
-            'g' => Self::Green,
-            _ => panic!("Invalid color {}", ch)
-        }
-    }
-}
+// impl Color {
+//     fn from_char(ch: char) -> Self {
+//         match ch {
+//             'w' => Self::White,
+//             'u' => Self::Blue,
+//             'b' => Self::Black,
+//             'r' => Self::Red,
+//             'g' => Self::Green,
+//             _ => panic!("Invalid color {}", ch)
+//         }
+//     }
+// }
 
-struct Towel {
-    stripes: Vec<Color>
-}
+// struct Towel {
+//     stripes: Vec<Color>
+// }
 
-impl FromStr for Towel {
-    type Err = String;
+// impl FromStr for Towel {
+//     type Err = String;
 
-    fn from_str(line: &str) -> Result<Self, Self::Err> {
-        let stripes: Vec<_> = line.chars().map(|ch| Color::from_char(ch)).collect();
-        Ok(Self {
-            stripes
-        })
-    }
-}
+//     fn from_str(line: &str) -> Result<Self, Self::Err> {
+//         let stripes: Vec<_> = line.chars().map(|ch| Color::from_char(ch)).collect();
+//         Ok(Self {
+//             stripes
+//         })
+//     }
+// }
 
-impl Towel {
-    fn can_be_made_from(&self, subs: &Vec<Towel>) -> bool {
-        todo!()
-    }
-}
+// impl Towel {
+//     fn can_be_made_from(&self, subs: &Vec<Towel>) -> bool {
+//         todo!()
+//     }
+// }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -54,11 +54,11 @@ fn main() {
         if sections.len() != 2 {
             panic!("Invalid number of sections")
         }
-        let available: Vec<Towel> = sections[0].split(", ").map(|s| s.parse().unwrap()).collect();
-        let designs: Vec<Towel> = sections[1].lines().map(|s| s.parse().unwrap()).collect();
+        let available: Vec<&str> = sections[0].split(", ").collect();
+        let designs: Vec<&str> = sections[1].lines().collect();
         let mut possible = 0;
         for d in &designs {
-            if d.can_be_made_from(&available) {
+            if can_be_made_from(&d, &available) {
                 possible += 1;
             }
         }
@@ -66,4 +66,8 @@ fn main() {
     } else {
         println!("Please provide 1 argument: Filename");
     }
+}
+
+fn can_be_made_from(target: &str, available: &Vec<&str>) -> bool {
+    todo!()
 }
