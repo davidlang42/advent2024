3f901f1695e817d90e93d6c686bc724edec34fa5
David Lang <davidlang42@gmail.com>
2024-12-22 19:10:43 +1100

    part2 test works but probably too slow

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -5,24 +5,27 @@ use std::ops::BitXor;
 #[derive(Debug)]
 struct Secret {
     number: usize,
-    price_deltas: Vec<i8>
+    prices: Vec<u8>,
+    deltas: Vec<i8>
 }
 
 impl Secret {
     fn new(number: usize) -> Self {
-        Self {
+        let mut s = Self {
             number,
-            price_deltas: Vec::new()
-        }
+            prices: Vec::new(),
+            deltas: Vec::new()
+        };
+        s.prices.push(s.price());
+        s
     }
 
     fn next_number(&mut self) {
-        let price_before = self.price();
         self.number = Self::prune(Self::mix(self.number * 64, self.number));
         self.number = Self::prune(Self::mix(self.number / 32, self.number));
         self.number = Self::prune(Self::mix(self.number * 2048, self.number));
-        let price_after = self.price();
-        self.price_deltas.push(price_before as i8 - price_after as i8);
+        self.prices.push(self.price());
+        self.deltas.push(self.prices[self.prices.len() - 1] as i8 - self.prices[self.prices.len() - 2] as i8); 
     }
 
     fn price(&self) -> u8 {
@@ -35,6 +38,15 @@ impl Secret {
 
     fn prune(a: usize) -> usize {
         a % 16777216
+    }
+
+    fn price_after_deltas(&self, deltas: [i8; 4]) -> Option<u8> {
+        for i in 0..(self.deltas.len() - 3) {
+            if self.deltas[i..(i+4)] == deltas {
+                return Some(self.prices[i+4]);
+            }
+        }
+        None
     }
 }
 
@@ -51,7 +63,30 @@ fn main() {
             }
         }
         let sum: usize = secrets.iter().map(|s| s.number).sum();
-        println!("Sum: {:?}", sum);
+        println!("Part1 sum: {:?}", sum);
+        let min = -9;
+        let max = 10;
+        let mut best_bananas: usize = 0;
+        let mut best = None;
+        for a in min..max {
+            for b in min..max {
+                for c in min..max {
+                    for d in min..max {
+                        let mut bananas: usize = 0;
+                        for s in &secrets {
+                            if let Some(price) = s.price_after_deltas([a,b,c,d]) {
+                                bananas += price as usize;
+                            }
+                        }
+                        if bananas > best_bananas {
+                            best_bananas = bananas;
+                            best = Some([a,b,c,d]);
+                        }
+                    }
+                } 
+            }
+        }
+        println!("Best bananas: {} ({:?})", best_bananas, best);
     } else {
         println!("Please provide 1 argument: Filename");
     }
