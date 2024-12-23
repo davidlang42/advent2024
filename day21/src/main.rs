fa9001c56147a78352f6f5c559336ee2f86cec24
David Lang <davidlang42@gmail.com>
2024-12-23 15:39:03 +1100

    start set up part1 as recursion

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::fs;
 use std::env;
+use directional::DirectionalKey;
+
 use crate::keypad::{Keypad, Key};
 use crate::code::Code;
 use crate::numeric::NumericKey;
@@ -18,27 +20,32 @@ fn main() {
             .expect(&format!("Error reading from {}", filename));
         let codes: Vec<Code<NumericKey>> = text.lines().map(|s| s.parse().unwrap()).collect();
         let mut sum = 0;
-        for code1 in codes {
-            let mut final_results = Vec::new();
-            let results1 = solve_for(&code1, "1");
-            for r1 in results1 {
-                let code2 = Code { keys: r1.movements.clone() };
-                let results2 = solve_for(&code2, "2");
-                for r2 in results2 {
-                    let code3 = Code { keys: r2.movements.clone() };
-                    final_results.append(& mut solve_for(&code3, "3"));
-                }
-            }
-            let shortest = final_results.iter().map(|r3| r3.movements.len()).min().unwrap();
-            let numeric_part = code1.numeric_part();
+        for code in codes {
+            let results = robot_indirection(&code);
+            let shortest: usize = results.iter().map(|r| r.movements.len()).min().unwrap();
+            let numeric_part = code.numeric_part();
             let complexity = numeric_part * shortest;
-            println!("Code: {}, Shortest: {}, Complexity: {}", code1, shortest, complexity);
+            println!("Code: {}, Shortest: {}, Complexity: {}", code, shortest, complexity);
             sum += complexity;
         }
         println!("Sum: {}", sum);
     } else {
         println!("Please provide 1 argument: Filename");
     }
+}
+
+fn robot_indirection(code: &Code<NumericKey>) -> Vec<Keypad<DirectionalKey>> {
+    let mut final_results = Vec::new();
+    let results1 = solve_for(code, "1");
+    for r1 in results1 {
+        let code2 = Code { keys: r1.movements.clone() };
+        let results2 = solve_for(&code2, "2");
+        for r2 in results2 {
+            let code3 = Code { keys: r2.movements.clone() };
+            final_results.append(& mut solve_for(&code3, "3"));
+        }
+    }
+    final_results
 }
 
 fn solve_for<K: Key>(code: &Code<K>, _log_description: &str) -> Vec<Keypad<K>> {
