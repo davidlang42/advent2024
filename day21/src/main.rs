d21ebc24003973524b5fb6f09bde2e839905b558
David Lang <davidlang42@gmail.com>
2024-12-23 15:52:47 +1100

    more

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -21,8 +21,8 @@ fn main() {
         let codes: Vec<Code<NumericKey>> = text.lines().map(|s| s.parse().unwrap()).collect();
         let mut sum = 0;
         for code in codes {
-            let results = robot_indirection(&code);
-            let shortest: usize = results.iter().map(|r| r.movements.len()).min().unwrap();
+            let results = robot_indirection_numeric(&code);
+            let shortest: usize = results.iter().map(|r| r.len()).min().unwrap();
             let numeric_part = code.numeric_part();
             let complexity = numeric_part * shortest;
             println!("Code: {}, Shortest: {}, Complexity: {}", code, shortest, complexity);
@@ -34,31 +34,36 @@ fn main() {
     }
 }
 
-fn robot_indirection(code: &Code<NumericKey>) -> Vec<Keypad<DirectionalKey>> {
+fn robot_indirection_numeric<K: Key>(code: &Code<K>) -> Vec<Vec<DirectionalKey>> {
     let mut final_results = Vec::new();
-    let results1 = solve_for(code, "1");
-    for r1 in results1 {
-        let code2 = Code { keys: r1.movements.clone() };
-        let results2 = solve_for(&code2, "2");
-        for r2 in results2 {
-            let code3 = Code { keys: r2.movements.clone() };
-            final_results.append(& mut solve_for(&code3, "3"));
-        }
+    let results = solve_for(code, "1");
+    for r in results {
+        let code2 = Code { keys: r };
+        final_results.append(&mut robot_indirection2(&code2));
     }
     final_results
 }
 
-fn solve_for<K: Key>(code: &Code<K>, _log_description: &str) -> Vec<Keypad<K>> {
+fn robot_indirection2(code: &Code<DirectionalKey>) -> Vec<Vec<DirectionalKey>> {
+    let mut final_results = Vec::new();
+    let results = solve_for(&code, "2");
+    for r in results {
+        let code3 = Code { keys: r };
+        final_results.append(&mut robot_indirection3(&code3));
+    }
+    final_results
+}
+
+fn robot_indirection3(code: &Code<DirectionalKey>) -> Vec<Vec<DirectionalKey>> {
+    solve_for(&code, "3")
+}
+
+fn solve_for<K: Key>(code: &Code<K>, _log_description: &str) -> Vec<Vec<DirectionalKey>> {
     //println!("[{}] Code: {}", log_description, code);
     let start = Keypad::<K>::new();
     let mut cache = HashMap::new();
-    let mut results = start.shortest_paths_to_code(&code, &mut cache);
+    let results = start.shortest_paths_to_code(&code, &mut cache);
     // filter out results which are no longer the shortest (due to combining with upstream results)
     let shortest = results.iter().map(|r| r.movements.len()).min().unwrap();
-    results = results.into_iter().filter(|r| r.movements.len() == shortest).collect();
-    // print shortest
-    //for result in &results {
-        //println!("[{}] Shortest ({}): {}", log_description, result.movements.len(), result.movements_string());
-    //}
-    results
+    results.into_iter().filter(|r| r.movements.len() == shortest).map(|r| r.movements).collect::<Vec<Vec<DirectionalKey>>>()
 }
