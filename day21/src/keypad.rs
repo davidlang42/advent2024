22227065ec37e473f96bffb79085bffa50d53d55
David Lang <davidlang42@gmail.com>
2024-12-23 14:45:40 +1100

    Revert "wip"
    This reverts commit 4fb12d6adee0ce7384eafdb8f75b7a847e81f59b.

--- day21/src/keypad.rs
+++ day21/src/keypad.rs
@@ -1,9 +1,6 @@
 use crate::Code;
 use crate::directional::{Direction, DirectionalKey};
-use std::cmp::Ordering;
 use std::hash::Hash;
-use std::ops::Add;
-use pathfinding::num_traits::Zero;
 use pathfinding::prelude::astar_bag;
 
 pub trait Key : Sized + Default + Clone + Hash + Eq + PartialEq {
@@ -21,85 +18,26 @@ pub trait Key : Sized + Default + Clone + Hash + Eq + PartialEq {
     }
 }
 
-#[derive(Clone)]
-struct Cost {
-    movements: Vec<DirectionalKey>
-}
-
-impl Cost {
-    fn from(directional_key: DirectionalKey) -> Self {
-        Self {
-            movements: vec![directional_key]
-        }
-    }
-
-    fn of(size: usize) -> Self {
-        let mut movements = Vec::new();
-        for _ in 0..size {
-            movements.push(DirectionalKey::Activate);
-        }
-        Self {
-            movements
-        }
-    }
-}
-
-impl PartialEq for Cost {
-    fn eq(&self, other: &Self) -> bool {
-        self.movements.len() == other.movements.len()
-    }
-}
-
-impl Eq for Cost {
-
-}
-
-impl PartialOrd for Cost {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.movements.len().partial_cmp(&other.movements.len())
-    }
-}
-
-impl Ord for Cost {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.movements.len().cmp(&other.movements.len())
-    }
-}
-
-impl Add for Cost {
-    fn add(self, mut rhs: Cost) -> Self {
-        let mut movements = self.movements.clone();
-        movements.append(&mut rhs.movements);
-        Self {
-            movements
-        }
-    }
-    
-    type Output = Self;
-}
-
-impl Zero for Cost {
-    fn zero() -> Self {
-        Self {
-            movements: Vec::new()
-        }
-    }
-
-    fn is_zero(&self) -> bool {
-        self.movements.len() == 0
-    }
-}
-
 #[derive(Clone, Hash, Eq, PartialEq, Debug)]
 pub struct Keypad<K: Key> {
-    pub current: K
+    pub current: K,
+    pub movements: Vec<DirectionalKey>//TODO exclude this from Eq/PartialEq
 }
 
 impl<K: Key> Keypad<K> {
     pub fn new() -> Self {
         Self{
-            current: K::default()
+            current: K::default(),
+            movements: Vec::new()
         }
+    }
+
+    pub fn _movements_string(&self) -> String {
+        let mut s = String::new();
+        for key in &self.movements {
+            s.push(key.to_char());
+        }
+        s
     }
 
     pub fn shortest_paths_to_code(&self, code: &Code<K>) -> Vec<Self> {
@@ -108,34 +46,33 @@ impl<K: Key> Keypad<K> {
 
     //TODO add caching if needed
     fn shortest_paths_to_code_recursive(start: &Self, code: &[K]) -> Vec<Self> {
-        todo!()
-        // let mut results = Vec::new();
-        // for mut result in Self::shortest_paths_to_key(start, &code[0]) {
-        //     result.movements.push(DirectionalKey::Activate);
-        //     if code.len() == 1 {
-        //         // this is a finished result, these will always be the shortest (due to astar_bag)
-        //         results.push(result);
-        //     } else {
-        //         // continue (recurisively) to the next key
-        //         results.append(&mut Self::shortest_paths_to_code_recursive(&result, &code[1..code.len()]));
-        //     }
-        // }
-        // // filter out results which are no longer the shortest (due to combining with upstream results)
-        // let shortest = results.iter().map(|r| r.movements.len()).min().unwrap();
-        // results.into_iter().filter(|r| r.movements.len() == shortest).collect()
+        let mut results = Vec::new();
+        for mut result in Self::shortest_paths_to_key(start, &code[0]) {
+            result.movements.push(DirectionalKey::Activate);
+            if code.len() == 1 {
+                // this is a finished result, these will always be the shortest (due to astar_bag)
+                results.push(result);
+            } else {
+                // continue (recurisively) to the next key
+                results.append(&mut Self::shortest_paths_to_code_recursive(&result, &code[1..code.len()]));
+            }
+        }
+        // filter out results which are no longer the shortest (due to combining with upstream results)
+        let shortest = results.iter().map(|r| r.movements.len()).min().unwrap();
+        results.into_iter().filter(|r| r.movements.len() == shortest).collect()
     }
 
     fn shortest_paths_to_key(start: &Self, key: &K) -> Vec<Self> {
-        let (results, cost) = astar_bag(start, |kp| kp.successors(), |kp| Cost::of(kp.current.minimum_distance_to(key)), |kp| kp.current == *key).expect("No solution");
+        let (results, _) = astar_bag(start, |kp| kp.successors(), |kp| kp.current.minimum_distance_to(key), |kp| kp.current == *key).expect("No solution");
         results.into_iter().map(|r| r.into_iter().last().unwrap()).collect()
     }
 
-    fn successors(&self) -> Vec<(Self, Cost)> {
+    fn successors(&self) -> Vec<(Self, usize)> {
         let mut v = Vec::new();
         for direction in self.valid_moves() {
             let mut clone = self.clone();
             clone.move_current(&direction);
-            v.push((clone, Cost::from(DirectionalKey::Move(direction))));
+            v.push((clone, 1));
         }
         v
     }
@@ -164,5 +101,6 @@ impl<K: Key> Keypad<K> {
             Direction::Left => self.current = self.current.key_left().unwrap(),
             Direction::Right => self.current = self.current.key_right().unwrap(),
         }
+        self.movements.push(DirectionalKey::Move(*direction))
     }
 }
--- day21/src/main.rs
+++ day21/src/main.rs
@@ -17,24 +17,23 @@ fn main() {
             .expect(&format!("Error reading from {}", filename));
         let codes: Vec<Code<NumericKey>> = text.lines().map(|s| s.parse().unwrap()).collect();
         let mut sum = 0;
-        todo!();
-        // for code1 in codes {
-        //     let mut final_results = Vec::new();
-        //     let results1 = solve_for(&code1, "1");
-        //     for r1 in results1 {
-        //         let code2 = Code { keys: r1.movements.clone() };
-        //         let results2 = solve_for(&code2, "2");
-        //         for r2 in results2 {
-        //             let code3 = Code { keys: r2.movements.clone() };
-        //             final_results.append(& mut solve_for(&code3, "3"));
-        //         }
-        //     }
-        //     let shortest = final_results.iter().map(|r3| r3.movements.len()).min().unwrap();
-        //     let numeric_part = code1.numeric_part();
-        //     let complexity = numeric_part * shortest;
-        //     println!("Code: {}, Shortest: {}, Complexity: {}", code1, shortest, complexity);
-        //     sum += complexity;
-        // }
+        for code1 in codes {
+            let mut final_results = Vec::new();
+            let results1 = solve_for(&code1, "1");
+            for r1 in results1 {
+                let code2 = Code { keys: r1.movements.clone() };
+                let results2 = solve_for(&code2, "2");
+                for r2 in results2 {
+                    let code3 = Code { keys: r2.movements.clone() };
+                    final_results.append(& mut solve_for(&code3, "3"));
+                }
+            }
+            let shortest = final_results.iter().map(|r3| r3.movements.len()).min().unwrap();
+            let numeric_part = code1.numeric_part();
+            let complexity = numeric_part * shortest;
+            println!("Code: {}, Shortest: {}, Complexity: {}", code1, shortest, complexity);
+            sum += complexity;
+        }
         println!("Sum: {}", sum);
     } else {
         println!("Please provide 1 argument: Filename");
@@ -42,16 +41,15 @@ fn main() {
 }
 
 fn solve_for<K: Key>(code: &Code<K>, _log_description: &str) -> Vec<Keypad<K>> {
-    todo!()
-    // //println!("[{}] Code: {}", log_description, code);
-    // let start = Keypad::<K>::new();
-    // let mut results = start.shortest_paths_to_code(&code);
-    // // filter out results which are no longer the shortest (due to combining with upstream results)
-    // let shortest = results.iter().map(|r| r.movements.len()).min().unwrap();
-    // results = results.into_iter().filter(|r| r.movements.len() == shortest).collect();
-    // // print shortest
-    // //for result in &results {
-    //     //println!("[{}] Shortest ({}): {}", log_description, result.movements.len(), result.movements_string());
-    // //}
-    // results
+    //println!("[{}] Code: {}", log_description, code);
+    let start = Keypad::<K>::new();
+    let mut results = start.shortest_paths_to_code(&code);
+    // filter out results which are no longer the shortest (due to combining with upstream results)
+    let shortest = results.iter().map(|r| r.movements.len()).min().unwrap();
+    results = results.into_iter().filter(|r| r.movements.len() == shortest).collect();
+    // print shortest
+    //for result in &results {
+        //println!("[{}] Shortest ({}): {}", log_description, result.movements.len(), result.movements_string());
+    //}
+    results
 }
