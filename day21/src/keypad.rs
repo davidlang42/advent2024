7894c74b51a08135a10ad3099b7ba3fbf1b98185
David Lang <davidlang42@gmail.com>
2024-12-24 11:32:17 +1100

    fix

--- day21/src/keypad.rs
+++ day21/src/keypad.rs
@@ -87,6 +87,7 @@ impl<K: Key> Keypad<K> {
             current: *start_key,
             movements: Vec::new()
         };
+        //TODO if filtering was unnessesary, do we really need all solutions?
         let (results, _) = astar_bag(&start, |kp| kp.successors(), |kp| kp.current.minimum_distance_to(key), |kp| kp.current == *key).expect("No solution");
         results.into_iter().map(|r| r.into_iter().last().unwrap()).collect()
     }
